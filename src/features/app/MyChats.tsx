de69b5350b1108888121d2dd8f7f8d35e3fce584
hiramgabriel-dev <hiram.gabriel@atratopago.com>
2025-01-03 15:35:20 -0700

    error page solved

--- src/features/app/MyChats.tsx
+++ src/features/app/MyChats.tsx
@@ -5,31 +5,33 @@ import { useEffect, useState } from "react";
 import ChatsCards from "./components/ChatsCards";
 
 function MyChats() {
-  const [chats, setChats] = useState();
+	const [chats, setChats] = useState();
 
-  useEffect(() => {
-    const data = Cookies.get("authToken");
-    const userData = jwtDecode<CustomJWTPayload>(data);
+	useEffect(() => {
+		const data = Cookies.get("authToken");
+		if (data) {
+			const userData = jwtDecode<CustomJWTPayload>(data);
+			if (userData) {
+				setChats(userData);
+			}
+		}
+	}, []);
 
-    if (userData) setChats(userData);
-  }, []);
-
-  useEffect(() => {
-    console.log(chats?.chats);
-  }, [chats]);
-  
-  return (
-    <>
-      <h1 className="text-2xl ml-3 mt-4">list to my chats</h1>
-      <div className="flex items-center justify-center h-screen">
-        {chats?.chats.length >= 1 ? (
-          <ChatsCards dataChats={chats?.chats}/>
-        ) : (
-          <h1 className="text-3xl font-semibold">You don't have chats</h1>
-        )}
-      </div>
-    </>
-  );
+	useEffect(() => {
+		console.log(chats?.chats);
+	}, [chats]);
+	return (
+		<>
+			<h1 className="text-2xl ml-3 mt-4">list to my chats</h1>
+			<div className="flex items-center justify-center h-screen">
+				{chats?.chats.length >= 1 ? (
+					<ChatsCards dataChats={chats?.chats} />
+				) : (
+					<h1 className="text-3xl font-semibold">You don't have chats</h1>
+				)}
+			</div>
+		</>
+	);
 }
 
 export default MyChats;
